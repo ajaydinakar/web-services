451a492953af94a219f9eb79bd8e37ee4b268ee0
ajaydinakar <ajaydinakar6@gmail.com>
2024-07-15 01:10:57 -0700

    adding wsdl first  approach customerorders project and sample code first soap webservice hellows

---
+++ hellowebservicejava8/src/main/java/com/ajay/ws/soap/Hellowebservicejava8Application.java
@@ -0,0 +1,13 @@
+package com.ajay.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellowebservicejava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellowebservicejava8Application.class, args);
+	}
+
+}
---
+++ wsdlfirstws/src/main/java/com/ajay/ws/soap/WsdlfirstwsApplication.java
@@ -0,0 +1,14 @@
+package com.ajay.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlfirstwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlfirstwsApplication.class, args);
+	}
+
+
+}
